694988d3639cc2ea89ce1eb3e574590181dc4460
LittleJake <465917717@qq.com>
2018-11-21 19:06:31 +0800

    admin节点管理页面

---
+++ apps/admin/controller/Node.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jake
+ * Date: 11/21/2018
+ * Time: 6:32 PM
+ */
+
+namespace app\admin\controller;
+use think\Controller;
+
+class Node extends Base
+{
+
+
+    public function infoAction(){
+        $type = 0;
+
+        if(!input('?id'))
+            $type = 1;
+
+        //防止SQL注入
+        $id = (int)input('id');
+
+        $info = db('ss_node')->where('id', $id) -> find();
+
+
+        $this->assign('type', $type);
+        $this->assign('info', $info);
+        $this->assign('page_title', '节点修改');
+        return $this ->fetch();
+    }
+
+    public function handlerAction(){
+
+    }
+}
