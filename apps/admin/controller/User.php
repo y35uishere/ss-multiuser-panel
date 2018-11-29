c2ae5db899a7c9dc41695c914195cd7e7462c700
LittleJake <465917717@qq.com>
2018-11-29 22:57:47 +0800

    增加账户停用功能

--- apps/admin/controller/User.php
+++ apps/admin/controller/User.php
@@ -30,7 +30,32 @@ class User extends Base
 		return $this -> fetch();
 	}
 
+    public function disableAction()
+    {
+        if(!input('?id'))
+            return $this->error('用户不存在','index/index', 0, 3);
 
+        $id = (int)input('id');
+
+        $db = db('user');
+        $query = $db -> where('uid', $id) -> find();
+
+        if($query['enable'] == 1){
+            $db ->where('uid', $id) ->update([
+                'enable' => 0
+            ]);
+
+            return $this->success('账户 '.  $query['user_name'].' 已禁用', url('admin/user/info', 'id='.$query['uid']), 0, 3);
+        }
+        else {
+            $db ->where('uid', $id) ->update([
+                'enable' => 1
+            ]);
+
+            return $this->success('账户 '.  $query['user_name'].' 已启用', url('admin/user/info', 'id='.$query['uid']), 0, 3);
+        }
+
+    }
 
 	
 	
